90ae55394aaa29f07852b43b17bb1611550f1e22
Spiros <geokh@hotmail.com>
2022-08-04 16:01:10 +0300

    Deserializing Objects

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,6 @@
+from dataclasses import fields
 from decimal import Decimal
+from multiprocessing import context
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 from store.models import Product, Collection
@@ -7,12 +9,28 @@ from store.models import Product, Collection
 # in the module (import serializers)
 # So the serializers module that we imported here it has a Serializer class
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+    # id = serializers.IntegerField()
+    # title= serializers.CharField(max_length=255)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title', 'unit_price', 'price_with_tax', 'collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.StringRelatedField()
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(), 
+    #     view_name='collection-detail',
+    # )
+
        
 
 
--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from turtle import title
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
@@ -10,15 +11,34 @@ from store import serializers
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+         serializer = ProductSerializer(data=request.data)
+        #  serializer.validated_data
+         return Response ('ok')
 
 @api_view()
 def product_detail(request, id):
     # return Response('ok')
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
+
+
+
+        
+
+
+
+
+
