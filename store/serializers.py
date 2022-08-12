b68f832889025473e7ac1be6e0358d6449636b08
Spiros <geokh@hotmail.com>
2022-08-12 10:46:35 +0300

    Creating Order Items and using transaction

--- store/serializers.py
+++ store/serializers.py
@@ -1,11 +1,13 @@
+import collections
 from dataclasses import field, fields
 from decimal import Decimal
+from django.db import transaction
 from itertools import product
 from multiprocessing import context
 from ssl import create_default_context
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
-from store.models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
+from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
 
 
 # this class will inherit this serializer class that is defined
@@ -156,17 +158,25 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data['cart_id'])
-        print(self.context['user_id'])
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
 
-        (customer, created)= Customer.objects.get_or_create(user_id=self.context['user_id'])
-        Order.objects.create(customer= customer)
+            (customer, created)= Customer.objects.get_or_create(user_id=self.context['user_id'])
+            order = Order.objects.create(customer= customer)
+            cart_items = CartItem.objects.select_related('product').filter(cart_id=cart_id)
 
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    unit_price = item.product.unit_price,
+                    quantity = item.quantity
+            ) for item in cart_items
+            ]
 
+            OrderItem.objects.bulk_create(order_items)
 
-
-
-
+            Cart.objects.filter(pk=cart_id).delete()
 
 
 
